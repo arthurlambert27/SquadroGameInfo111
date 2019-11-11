640aa299a25d910bc61fa6a0208d2ade1887e1e4
Arthur Lambert <arthur@pop-os.localdomain>
2019-11-11 12:43:49 +0100

    Ajout de la fonction ptnDeMouvement permetant de calculer le point de mouvement d'un Pion et ajout de la variable vector<int> etatPions permettant de savoir si un pion fait un allé ou un retour.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-
+#include <tuple>
 
 
 
@@ -31,57 +31,82 @@ std::vector<int> posPions(std::vector<std::vector<int>> plateau){
 	}
 
 /*
+* Permet le nombre de point de mouvement d'une pièce
+* @param noPion qui est le numéro du pion
+* @param joueur qui est le numéro du joueur
+* @param etatPions pour savoir si les pions sont dans la phase d'allé ou de retour.
+* @return le nombre de point de mouvement d'une pièce, négatif si pour le retour
+*/
+//TODO: Avec étatPions, continuer la fonction pour qu'elle comprenne le retour.
+int ptnDeMouvement(int noPion, int joueur, std::vector<int> etatPions){
+	int ptnMouvement = 0;
+	if(joueur == 1){
+
+
+
+		if(noPion == 1)
+			ptnMouvement= 1;
+		else if(noPion == 2)
+			ptnMouvement = 3;
+		else if(noPion == 3)
+			ptnMouvement = 2;
+		else if(noPion == 4)
+			ptnMouvement = 3;
+		else if(noPion ==4)
+			ptnMouvement = 1;
+		}
+
+		if(joueur == 2){
+
+			if(noPion == 1)
+			ptnMouvement = 3;
+		else if(noPion == 2)
+			ptnMouvement = 1;
+		else if(noPion == 3)
+			ptnMouvement = 2;
+		else if(noPion == 4)
+			ptnMouvement = 1;
+		else if(noPion ==4)
+			ptnMouvement = 3;
+		}
+		return ptnMouvement;
+}
+
+/*
  * Permet de bouger un jeton
  * @param plateau qui est l'état du jeu a un certain moment
  * @param joueur qui est le joueur a déplacer
  * @param noPion qui jeton qui est le jeton a déplacé (de haut en bas pour le joueur 1 et de gauche a droite pour le joueur 2)
  * @return plateau qui retourne l'état du jeu suite au déplacement.
  * */
- std::vector<std::vector<int>> deplace(std::vector<std::vector<int>> plateau, int joueur, int noPion ){
+ std::tuple<std::vector<std::vector<int>>,std::vector<int>> deplace(std::vector<std::vector<int>> plateau, std::vector<int> etatPions, int joueur, int noPion ){
 	 std::vector<int> pos = posPions(plateau);
 		if(joueur == 1){
-			
-			
-			int ptnMouvement = 0;
-			if(noPion == 1)
-				ptnMouvement= 1;
-			else if(noPion == 2)
-				ptnMouvement = 3;
-			else if(noPion == 3)
-				ptnMouvement = 2;
-			else if(noPion == 4)
-				ptnMouvement = 3;
-			else if(noPion ==4)
-				ptnMouvement = 1;
-			
-			
+
+
+			int ptnMouvement = ptnDeMouvement(noPion, 1, etatPions);
+
+
 			int posy = pos[noPion * 2 - 2];
 			int posx = pos[noPion * 2 -1];
-			
+
 			plateau[posy][posx] = 0;
-			
+
 			plateau[posy][posx+ptnMouvement] = 1;
-			
+
 			}
 			if(joueur == 2){
-				int ptnMouvement = 0;
-				if(noPion == 1)
-				ptnMouvement = 3;
-			else if(noPion == 2)
-				ptnMouvement = 1;
-			else if(noPion == 3)
-				ptnMouvement = 2;
-			else if(noPion == 4)
-				ptnMouvement = 1;
-			else if(noPion ==4)
-				ptnMouvement = 3;
+				int ptnMouvement = ptnDeMouvement(noPion, 2, etatPions);
+
 			int posy = pos[10 + noPion * 2 - 2];
 			int posx = pos[10 + noPion * 2 -1];
 			plateau[posy][posx] = 0;
-			
+
 			plateau[posy-ptnMouvement][posx] = 2;
 				}
-		return plateau;
+			std::tuple<std::vector<std::vector<int>>,std::vector<int>> plateau1 (plateau, etatPions);
+
+		return plateau1;
 	 }
 
 
@@ -131,13 +156,19 @@ std::vector<std::vector<int>> creationPlateauInitial(){
 
 int main(){
     std::vector<std::vector<int>> plateau = creationPlateauInitial();
-    plateau = deplace(plateau, 1, 2);
-    plateau = deplace(plateau, 2, 1);
-    affichePlateau(plateau);
-	std::vector<int> pos = posPions(plateau);
-	
-	for(auto i : pos){
-		std::cout << i << ";";
+		std::vector<int> etatPions;
+		etatPions = std::vector<int>(10);
+		for(int i =0; i<etatPions.size(); i++){
+			etatPions[i] = 0;
 		}
+
+		std::tuple<std::vector<std::vector<int>>, std::vector<int>> plateau1;
+    plateau1 = deplace(plateau, etatPions, 1, 2);
+    plateau = std::get<0>(plateau1);
+		etatPions = std::get<1>(plateau1);
+
+		affichePlateau(plateau);
+
+
     return 0;
 }
