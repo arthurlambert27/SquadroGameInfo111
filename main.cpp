59e5023d59f25ec8f221e00a909947638fda00b6
Arthur Lambert <arthur@pop-os.localdomain>
2019-11-15 11:35:43 +0100

    Ajout de la fonction affichage version 2

--- main.cpp
+++ main.cpp
@@ -387,6 +387,42 @@ void affichePlateau(std::vector<std::vector<int>> plateau){
 }
 
 /*
+* Affiche le plateau du jeu
+* @param plateau Plateau du jeu a afficher
+*/
+void affichePlateauV2(std::vector<std::vector<int>> plateau){
+	std::string affichage;
+	affichage = "|----------------------------------|\n";
+
+	for(int i = 0; i<7; i++){
+		for(int j = 0; j<7; j++){
+			if(plateau[i][j] == 1 or plateau[i][j] == 2){
+
+				affichage = affichage + "|" + std::to_string(plateau[i][j]) + "   ";
+			}
+			else{
+				affichage = affichage  + "|    "  ;
+			}
+		}
+
+		affichage = affichage + "|\n";
+		for(int a = 0; a<3; a++){
+			for(int r = 0; r<7; r++){
+				affichage = affichage + "|    ";
+			}
+			affichage = affichage + "|\n";
+		}
+		affichage = affichage + "|----------------------------------|\n";
+	}
+	std::cout << affichage;
+}
+
+/*
+* Permet de créer le plateau initial
+* @return le plateau
+*/
+
+/*
 * Permet de créer le plateau initial
 * @return le plateau
 */
@@ -462,7 +498,7 @@ int main(){
 
 
 
-		affichePlateau(plateau);
+		affichePlateauV2(plateau);
 
 
     return 0;
